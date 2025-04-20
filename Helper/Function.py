55451c28d1577e3f22c30ba597743095cedf0093
Zhi Wei Kau <zhiwei101191@gmail.com>
2025-04-20 12:04:53 +0800

    Update Function.py

--- Helper/Function.py
+++ Helper/Function.py
@@ -9,7 +9,6 @@ from rapidfuzz import fuzz
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
-import en_core_web_md
 
 def combine_data(file_list, path_way, output_file="Coursera_combined_data.csv"):
     combine_df = pd.DataFrame()
@@ -81,7 +80,7 @@ def load_job_embeddings():
 
 # Function to extract skills from the Job Responsibility column
 def extract_skills_from_job_responsibility(job_responsibility_text):
-    nlp = en_core_web_md.load()
+    nlp = spacy.load("en_core_web_md")
     
     known_skills = [
         "python", "r", "sql", "java", "scala", "c++", "matlab", "julia", "mysql", 
@@ -128,7 +127,7 @@ def extract_skills_from_job_responsibility(job_responsibility_text):
 
 # Function to extract skills from the Resume text
 def extract_skills_from_resume(resume_text):
-    nlp = en_core_web_md.load()
+    nlp = spacy.load("en_core_web_md")
     
     known_skills = [
         "python", "r", "sql", "java", "scala", "c++", "matlab", "julia", "mysql", 
